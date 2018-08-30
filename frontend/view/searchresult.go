d9b6d5daad6f4cfd4ba1dc20a1c9e260c99f7511
liaan <837448792@qq.com>
2018-08-30 18:18:58 +0800

    Update searchresult.go

--- frontend/view/searchresult.go
+++ frontend/view/searchresult.go
@@ -10,10 +10,28 @@ type SearchResultView struct {
 	template *template.Template
 }
 
+//减法，为了在模板里用减1
+func Sub(a, b int) int {
+	return a - b
+}
+
+//加法，为了在模板里用加1
+func Add(a, b int) int {
+	return a + b
+}
+
 func CreateSearchResultView(filename string) SearchResultView {
 
+	funcMaps := template.FuncMap{"Add": Add, "Sub": Sub}
+	//template := template.Must(template.ParseFiles(filename)).Funcs(funcMaps)
+	template, err := template.New("template.html").Funcs(funcMaps).ParseFiles(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return SearchResultView{
-		template.Must(template.ParseFiles(filename)),
+		template,
 	}
 }
 
